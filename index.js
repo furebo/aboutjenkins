9079de9434c63d06ee5fe33c778b8dad4998d29d
furebo <furebodidace582@gmail.com>
2022-03-29 11:02:26 +0200

    dotenv installed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
+const port = process.env.PORT || 3000
 
-app.listen(3000,()=>console.log('app listing on port 3000 .'));
+
+app.listen(3000,()=>console.log(`app listing on port 3000 ${port}.`));
